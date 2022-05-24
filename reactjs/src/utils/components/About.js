e0bf6d4960603774830d02a6f87e2c649bb9a453
Olga Trubetckaia <olga.trubetckaia@mail.ru>
2022-05-24 15:51:27 +0300

    'About' component

---
+++ reactjs/src/utils/components/About.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function About() {
+    return (
+      <div>
+          <h3>Just Coffee</h3>
+          <p>The place is intended to provide a nice relaxing atmosphere for anyone who needs it.
+          Just spend some time with your own thoughts, friends or family.</p>
+          <h3>Coffee - because adulting is hard</h3>
+      </div>
+    );
+}
+
+export default About;
